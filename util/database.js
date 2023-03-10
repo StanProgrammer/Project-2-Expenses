3e5fbac7501a693c77b42d55260910014d24f9f2
AtibProgrammer <atibkhan392@gmail.com>
2023-03-10 14:33:38 +0530

    Frontend 2

--- util/database.js
+++ util/database.js
@@ -1,8 +1,7 @@
 const Sequelize  = require('sequelize');
 
-
 const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.USER_NAME}`, `${process.env.USER_PASSWORD}`, {
-    host: 'localhost',
+    host: `${process.env.DB_HOST}`,
     dialect: 'mysql'
   });
   module.exports=sequelize

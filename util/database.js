3b25eb7c50fcb8512c78663efb5ef1d1d922a156
AtibProgrammer <atibkhan392@gmail.com>
2023-03-11 11:25:05 +0530

    cc2

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
 const Sequelize  = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
-
-const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.USER_NAME}`, `${process.env.USER_PASSWORD}`, {
+const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.DB_USERNAME}`,`${process.env.DB_PASSWORD}`, {
     host: `${process.env.DB_HOST}`,
     dialect: 'mysql'
   });
-  console.log(process.env.USER_PASSWORD);
   module.exports=sequelize

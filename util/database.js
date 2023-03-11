05cbcfb55c4926c2c687b9c0b0bc472a555bfd47
AtibProgrammer <atibkhan392@gmail.com>
2023-03-11 11:17:38 +0530

    Done

--- util/database.js
+++ util/database.js
@@ -1,7 +1,10 @@
 const Sequelize  = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.USER_NAME}`, `${process.env.USER_PASSWORD}`, {
     host: `${process.env.DB_HOST}`,
     dialect: 'mysql'
   });
+  console.log(process.env.USER_PASSWORD);
   module.exports=sequelize

979277849180624c303d85a629155df8d0b17c08
Atib Khan <atibkhan392@gmail.com>
2024-08-08 22:04:27 +0530

    version 2.0 development started

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-const express = require('express');
-const fs = require('fs')
-const path = require('path')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const User = require('./models/users')
-const Expense = require('./models/expense')
-const Order = require('./models/order');
-const Forgotpassword = require('./models/forgotpassword');
-const DownloadUrl = require('./models/downloadUrl');
+const User = require("./models/users");
+const Expense = require("./models/expense");
+const Order = require("./models/order");
+const Forgotpassword = require("./models/forgotpassword");
+const DownloadUrl = require("./models/downloadUrl");
 
-const userRoutes = require('./routes/userRoutes')
-const purchaseRoutes = require('./routes/purchaseRoutes');
-const premiumRoutes = require('./routes/premiumRoutes');
-const forgotpasswordRoutes = require('./routes/forgotRoutes');
+const userRoutes = require("./routes/userRoutes");
+const purchaseRoutes = require("./routes/purchaseRoutes");
+const premiumRoutes = require("./routes/premiumRoutes");
+const forgotpasswordRoutes = require("./routes/forgotRoutes");
 
-
-const sequelize = require('./util/database');
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
-);
+const sequelize = require("./util/database");
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
 
 const app = express();
 // app.use(helmet())
@@ -33,20 +29,20 @@ app.use(
   helmet({
     contentSecurityPolicy: false,
   })
-)
+);
 app.use(cors());
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 // app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(userRoutes)
-app.use('/user', userRoutes);
-app.use('/purchase',purchaseRoutes)
-app.use('/premium', premiumRoutes);
-app.use('/password', forgotpasswordRoutes);
-User.hasMany(Expense)
-Expense.belongsTo(User)
+app.use(userRoutes);
+app.use("/user", userRoutes);
+app.use("/purchase", purchaseRoutes);
+app.use("/premium", premiumRoutes);
+app.use("/password", forgotpasswordRoutes);
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 User.hasMany(Order);
 Order.belongsTo(User);
@@ -57,9 +53,13 @@ Forgotpassword.belongsTo(User);
 User.hasMany(DownloadUrl);
 DownloadUrl.belongsTo(User);
 
-sequelize.sync()
-.then(
-app.listen(process.env.PORT,()=>{
-  console.log('no error');
-}))
-.catch(err=>{console.log(err);})
+sequelize
+  .sync()
+  .then(
+    app.listen(process.env.PORT, () => {
+      console.log("no error");
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,8 +1,11 @@
-const Sequelize  = require('sequelize');
-const dotenv = require('dotenv');
-dotenv.config();
-const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.DB_USERNAME}`,`${process.env.DB_PASSWORD}`, {
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  `${process.env.DATABASE_NAME}`,
+  `${process.env.DB_USERNAME}`,
+  `${process.env.DB_PASSWORD}`,
+  {
     host: `${process.env.DB_HOST}`,
-    dialect: 'mysql'
-  });
-  module.exports=sequelize
+    dialect: "mysql",
+  }
+);
+module.exports = sequelize;
